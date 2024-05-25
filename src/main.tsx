28e6207473d5262785bd7d1a32c58c9460937a4d
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-05-25 07:43:26 -0300

    chore: update Tailwind configuration and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from './components/theme-provider'
 import Root from './components/ui/root'
+import { TooltipProvider } from './components/ui/tooltip'
 import './index.css'
 import App from './pages/app'
 
@@ -24,6 +26,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
