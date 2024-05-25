5bfe3e74cca0feee6e0a0299f1a461b3f8055ca5
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-05-25 18:16:21 -0300

    chore: Update .env.example and .gitignore

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_IOT_API: string
+  readonly VITE_IOT_EXPRESS_API: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
