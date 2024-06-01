4c21593c5b62acdecf167cc8741b1af344dc0436
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-06-01 17:55:47 -0300

    feat: Add authentication routes and pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './components/theme-provider'
 import Root from './components/ui/root'
+import { Toaster } from './components/ui/sonner'
 import { TooltipProvider } from './components/ui/tooltip'
+import { AuthProvider } from './contexts/auth-context'
 import './index.css'
 import App from './pages/app'
+import LoginPage from './pages/auth/login-page'
+import SignUpPage from './pages/auth/signup-page'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/auth',
+    element: <LoginPage />,
+  },
+  {
+    path: '/auth/signup',
+    element: <SignUpPage />,
   },
   {
     path: '/dashboard',
@@ -28,7 +40,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <TooltipProvider>
-        <RouterProvider router={router} />
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </AuthProvider>
       </TooltipProvider>
     </ThemeProvider>
   </React.StrictMode>,
