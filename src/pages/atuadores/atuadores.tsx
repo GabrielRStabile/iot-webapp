2f5ae5b5f2f2458d44815544684d10928879d005
guilhermeshibuya <gyudishibuya@gmail.com>
2024-06-04 17:46:26 -0300

    remvoed check route from dispositivos page

--- src/pages/atuadores/atuadores.tsx
+++ src/pages/atuadores/atuadores.tsx
@@ -1,20 +1,11 @@
-import { useLocation } from 'react-router-dom'
 import AtuadoresListPage from './atuadores-list'
 
 const AtuadoresPage = () => {
-  const path = useLocation()
-  // const params = useParams()
-
-  const checkRoute = () => {
-    const { pathname } = path
-
-    if (pathname === '/dashboard/atuadores') {
-      return <AtuadoresListPage />
-    }
-  }
   return (
     <>
-      <div className="p-4 h-full">{checkRoute()}</div>
+      <div className="p-4 h-full">
+        <AtuadoresListPage />
+      </div>
     </>
   )
 }
