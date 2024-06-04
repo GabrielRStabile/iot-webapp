27746b20a47d0fd554c3e33ad3da333cb067f253
guilhermeshibuya <gyudishibuya@gmail.com>
2024-06-04 00:06:40 -0300

    add atuadores route

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import LoginPage from './pages/auth/login-page'
 import SignUpPage from './pages/auth/signup-page'
 import DispositivosPage from './pages/dispositivos/dispositivos'
 import { APIProvider } from '@vis.gl/react-google-maps'
+import AtuadoresPage from './pages/atuadores/atuadores'
 
 const router = createBrowserRouter([
   {
@@ -50,6 +51,10 @@ const router = createBrowserRouter([
             path: ':dispositivoId/edit',
           },
         ],
+      },
+      {
+        path: 'atuadores',
+        element: <AtuadoresPage />,
       },
     ],
   },
---
+++ src/pages/atuadores/atuadores.tsx
@@ -0,0 +1,9 @@
+const AtuadoresPage = () => {
+  return (
+    <>
+      <div className="bg-neutral-50 p-4 h-full"></div>
+    </>
+  )
+}
+
+export default AtuadoresPage
