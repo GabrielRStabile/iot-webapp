0c4cfbf15cb3d801adc23a71662271c1f9152089
guilhermeshibuya <gyudishibuya@gmail.com>
2024-06-04 14:40:35 -0300

    delete atuador funcionando

--- src/domain/atuador/atuador-queries.ts
+++ src/domain/atuador/atuador-queries.ts
@@ -1,11 +1,16 @@
 import fetchClient from '@/services/fetch-client'
 import { GetAtuador } from './get-atuador-dto'
 
-const baseUrl = 'http://localhost:8080/atuador'
+const baseUrl = `${import.meta.env.VITE_IOT_API}/atuador`
 
 const getAtuadores = async (): Promise<GetAtuador[]> => {
   const { data } = await fetchClient().get(baseUrl)
   return data
 }
 
-export { getAtuadores }
+const deleteAtuadorById = async (atuadorId: number): Promise<number> => {
+  const { status } = await fetchClient().delete(`${baseUrl}/${atuadorId}`)
+  return status
+}
+
+export { getAtuadores, deleteAtuadorById }
